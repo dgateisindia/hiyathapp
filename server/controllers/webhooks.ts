5a54fef1d6a01fb9bee34d58d0efbc97e1a75a6b
Abdul Shahul <abdul61643@gmail.com>
2026-05-21 16:45:28 +0530

    clerk integrated in backend server

---
+++ server/controllers/webhooks.ts
@@ -0,0 +1,41 @@
+import { verifyWebhook } from "@clerk/express/webhooks"
+import { Response, Request } from "express"
+import User from "../models/user"
+
+export const clerkWebhook = async (req: Request, res: Response) => {
+    try {
+        const evt = await verifyWebhook(req)
+
+        if (evt.type === 'user.created' || evt.type === 'user.updated') {
+
+            const existingUser = await User.findOne({
+                clerkId: evt.data.id
+            })
+
+            const userData = {
+                clerkId: evt.data.id,
+                email: evt.data?.email_addresses[0].email_address,
+                name: evt.data?.first_name + " " + evt.data?.last_name,
+                image: evt.data?.image_url,
+            }
+
+            if (existingUser) {
+                await User.findOneAndUpdate(
+                    { clerkId: evt.data.id },
+                    userData
+                )
+            } else {
+                await User.create(userData)
+            }
+        }
+
+        return res.json({
+            success: true,
+            message: 'webhook received'
+        })
+
+    } catch (err) {
+        console.error('Error verifying webhook:', err)
+        return res.status(400).send('Error verifying webhook')
+    }
+}
--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,14 @@ import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware())
 
 const port = process.env.PORT || 3000;
 
